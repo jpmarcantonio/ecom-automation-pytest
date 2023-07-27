0f5c1e38b9eed54d2f3e1b9f7efe6b1ae7fc0839
JP Marcantonio <marcantoniojp@gmail.com>
2023-07-26 23:03:20 -0400

    wip

---
+++ variables_local.bat
@@ -0,0 +1,16 @@
+# to set these variables to environment run `variables_local.bat`. Just type the name and hit ENTER.
+
+# url setting
+set BASE_URL=http://127.0.0.1:8888/localdemostore/
+
+# browser setting
+set BROWSER=firefox
+
+# database configs
+set DB_PORT=8889
+set DB_HOST=localhost
+set DB_DATABASE=localdemostore
+set DB_TABLE_PREFIX=wp_
+
+
+set PYTHONPATH=C:\Users\user\Path\To\Project\ecom-automation-pytest
